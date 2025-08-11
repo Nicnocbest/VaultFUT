3de4b256766be0cb4597e0cc30650bbe5e694425
Nicnocbest <nicolasmoryson2012@gmail.com>
2025-08-12 01:49:18 +0200

    AchievementsTab.tsx

---
+++ AchievementsTab.tsx
@@ -0,0 +1,7 @@
+export default function AchievementsTab() {
+  return (
+    <div className="rounded-lg p-4 bg-white/5 border border-white/10">
+      🏆 Achievements – milestones coming soon
+    </div>
+  );
+}
