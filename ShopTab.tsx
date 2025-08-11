63cb1b4a6f1c496e4f2b17500f68ecd17770f35f
Nicnocbest <nicolasmoryson2012@gmail.com>
2025-08-12 01:55:37 +0200

    ShopTab.tsx

---
+++ ShopTab.tsx
@@ -0,0 +1,7 @@
+export default function ShopTab() {
+  return (
+    <div className="rounded-lg p-4 bg-white/5 border border-white/10">
+      🛒 Shop – coin packages & orders coming soon
+    </div>
+  );
+}
