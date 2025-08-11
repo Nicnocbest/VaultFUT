53acda941eaf870e76c23c4c8af03889e3431baa
Nicnocbest <nicolasmoryson2012@gmail.com>
2025-08-12 01:52:22 +0200

    LeaderboardTab.tsx

---
+++ LeaderboardTab.tsx
@@ -0,0 +1,7 @@
+export default function LeaderboardTab() {
+  return (
+    <div className="rounded-lg p-4 bg-white/5 border border-white/10">
+      🏅 Leaderboard – coming soon
+    </div>
+  );
+}
