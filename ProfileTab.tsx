6858f28ad1a727d513576a180871384c6dd02c56
Nicnocbest <nicolasmoryson2012@gmail.com>
2025-08-12 01:54:34 +0200

    ProfileTab.tsx

---
+++ ProfileTab.tsx
@@ -0,0 +1,7 @@
+export default function ProfileTab() {
+  return (
+    <div className="rounded-lg p-4 bg-white/5 border border-white/10">
+      👤 Profile – avatar & email coming soon
+    </div>
+  );
+}
